77ab3f6cfe3a8147236754e948aefad223147272
adrian-git-hub-user <adrian.ronayne@gmail.com>
2012-10-11 23:17:11 +0100

    Update

--- src/FileMatcher.scala
+++ src/FileMatcher.scala
@@ -4,6 +4,28 @@
 
 object FileMatcher {
   
-  private def filesHere = (new java.io.File(".")).listFiles()
+  private def filesHere = (new java.io.File("d:\\downloads")).listFiles()
+  
+  /**
+   * filesMatching takes a function argument called matcher.
+   * The function matcher takes a String parameter and returns a boolean
+   */
+  private def filesMatching(matcher: (String) => Boolean) = 
+    for(file <- filesHere; if matcher(file.getName))
+    	yield file
+
+  def filesEnding(query: String) = 
+    filesMatching(_.endsWith(query))
+    
+    def main(args:Array[String]) {
+     // println("here")
+      val pdfFileArray = filesEnding(".pdf")
+      val dir = new java.io.File("d:\\downloads\\nameoffolder");  
+      dir.mkdir();
+      
+      for(fileName <- pdfFileArray){
+        println(fileName)
+      }
+    }
 
 }
